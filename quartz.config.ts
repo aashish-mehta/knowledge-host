58cfb9d7b6a4cc66951997e46943bcfc3d276540
Aashish Mehta <43656153+aashish1498@users.noreply.github.com>
2025-05-20 22:45:02 +0100

    Quartz sync: May 20, 2025, 10:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Aashish Mehta Knowledge Base",
+    pageTitle: "Aashish Mehta",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
